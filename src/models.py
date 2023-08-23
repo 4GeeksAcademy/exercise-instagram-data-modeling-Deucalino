b2ea8786bbfef2fe0fd913594433634e3a91d8ce
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-08-23 17:55:23 +0000

    code set, but with importing troubles

--- src/models.py
+++ src/models.py
@@ -7,23 +7,39 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    username = Column(String(250), primary_key=True)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Followers(Base):
+    __tablename__ = 'followers'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    f_username = Column(String(250), primary_key=True)
+    f_name = Column(String(250), nullable=False)
+    f_lastname = Column(String(250), nullable=False)
+
+    class MyProfile(Base):
+        __tablename__='myprofile'
+        post_id=Column(Integer, primary_key=True)
+        post=Column(String(450), ForeignKey('post.username'))
+        post=relationship(User)
+        comment_id=Column(Integer, primary_key=True)
+        comment=Column(String(450), ForeignKey('comment.username'))
+        comment=relationship(User)
+        stories_id=Column(Integer, primary_key=True)
+        stories=Column(String(450), ForeignKey('stories.username'))
+    class Favorites(Base):
+        best_stories=Column(String(450), ForeignKey('stories.stories_id'))
+        best_post=Column(String(450), ForeignKey('post.post_id'))
+        user=relationship(User)
+        
 
     def to_dict(self):
         return {}
